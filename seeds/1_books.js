a18188d6ca5d88a0df2adeb0c398820204d0f95b
Jamieson Bates <jamiesonbates@gmail.com>
2017-01-04 14:37:05 -0800

    Add seed file

---
+++ seeds/1_books.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const data = require('../data');
+
+exports.seed = function(knex) {
+  return knex('books').del()
+    .then(() => {
+      return knex('books').insert(data);
+    })
+    // .then(() => {
+    //   return knex.raw('SELECT setval("books_id_seq", (SELECT MAX(id) FROM books));');
+    // });
+};
